c7ea29d28ccde0f72b2db5af2fe93080c3c866fb
Rahman <rahmanromli@yahoo.com>
2017-09-17 14:42:13 +0800

    Add firebase config

---
+++ src/Config/Config.js
@@ -0,0 +1,10 @@
+// console.log('env', process.env)
+// Initialize Firebase
+export const DB_CONFIG = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DB_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID
+}
