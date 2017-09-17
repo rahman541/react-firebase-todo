e698aaa930cc92ec69f031a176d5e99be1d5321b
Rahman <rahmanromli@yahoo.com>
2017-09-17 10:13:35 +0800

    Add note component to app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Note from './Note/Note'
 
 class App extends Component {
   render() {
     return (
-      <h1>React & Firebase To Do List</h1>
+      <div>
+        <h1>React & Firebase To Do List</h1>
+        <Note />
+      </div>
     );
   }
 }
--- src/Note/Note.jsx
+++ src/Note/Note.jsx
@@ -3,16 +3,22 @@ import './Note.css'
 import PropTypes from 'prop-types'
 
 class Note extends Component {
-	
+
 	constructor (props) {
 		super(props)
+		this.message = 'Hello from note component'
 	}
 
 	render (props) {
 		return (
 				<div>
-					<h1>Hello world</h1>
+					<h1>{this.message}</h1>
 				</div>
 			)
 	}
 }
+
+Note.proppTypes = {
+}
+
+export default Note
