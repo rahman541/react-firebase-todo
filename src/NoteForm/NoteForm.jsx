943eef204b212d6a80581e6688043d80c9fb94f1
Rahman <rahmanromli@yahoo.com>
2017-09-17 11:08:01 +0800

    Update note form: add handler user input

--- src/NoteForm/NoteForm.jsx
+++ src/NoteForm/NoteForm.jsx
@@ -6,16 +6,34 @@ class NoteForm extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-
+			newNoteContent: ''
 		}
+		this.handleUserInput = this.handleUserInput.bind(this)
+		this.writeNote = this.writeNote.bind(this)
+	}
+
+	handleUserInput (e) {
+		console.log(e.target.value)
+		this.setState({
+			newNoteContent: e.target.value,
+		})
+	}
+
+	writeNote () {
+		this.setState({
+			newNoteContent: ''
+		})
 	}
 
 	render () {
 		return (
 			<div className="formWrapper">
 				<input className="noteInput"
-					placeholder="Write a note ..." />
-				<button className="noteButton">
+					placeholder="Write a note ..."
+					onChange={this.handleUserInput}
+					value={this.state.newNoteContent}
+					/>
+				<button className="noteButton" onClick={this.writeNote>
 					Add Note
 				</button>
 			</div>
