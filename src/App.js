cca706cbf82a13f059a561a6faa34bb67cda9618
Rahman <rahmanromli@yahoo.com>
2017-09-17 10:27:47 +0800

    Display note

--- src/App.js
+++ src/App.js
@@ -4,11 +4,34 @@ import './App.css';
 import Note from './Note/Note'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      notes: [
+        { id: 1, noteContent: 'Note 1' },
+        { id: 2, noteContent: 'Note 2' }
+      ]
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h1>React & Firebase To Do List</h1>
-        <Note />
+      <div className="notesWrapper">
+        <div className="notesHeader">
+          <div className="heading">React &amp; Firebase To Do List</div>
+        </div>
+        <div className="notesBody">
+          {
+            this.state.notes.map((note) => {
+              return (
+                <Note noteContent={note.noteContent} noteId={note.id} key={note.id}/>
+              )
+            })
+          }
+        </div>
+        <div className="notesFooter">
+          Footer will go here...
+        </div>
       </div>
     );
   }
