efb0e97e9d40c7a872a6b5c0405e9007c1be4dad
Rahman <rahmanromli@yahoo.com>
2017-09-17 10:08:43 +0800

    Add note components

---
+++ src/Note/Note.jsx
@@ -0,0 +1,18 @@
+import React , { Component } from 'react'
+import './Note.css'
+import PropTypes from 'prop-types'
+
+class Note extends Component {
+	
+	constructor (props) {
+		super(props)
+	}
+
+	render (props) {
+		return (
+				<div>
+					<h1>Hello world</h1>
+				</div>
+			)
+	}
+}
